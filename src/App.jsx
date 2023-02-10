9fb31473c30ef18e61052eec89941665db960b0d
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-10 16:11:04 -0300

    updated app and Product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './assets/components/Header/Header';
@@ -5,12 +6,22 @@ import Contact from './Pages/Contact/Contact';
 import Products from './Pages/Products/Products';
 
 function App() {
+   const [datas, setDatas] = React.useState([]);
+
+   React.useEffect(() => {
+      fetch('https://ranekapi.origamid.dev/json/api/produto')
+         .then((res) => res.json())
+         .then((data) => setDatas(data));
+   }, []);
+
+   console.log(datas);
+
    return (
       <>
          <BrowserRouter>
             <Header />
             <Routes>
-               <Route path="/" element={<Products />} />
+               <Route path="/" element={<Products datas={datas} />} />
                <Route path="contact" element={<Contact />} />
             </Routes>
          </BrowserRouter>
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Wrap } from '../../style/Styles.style';
+import Card from './component/Card';
 import { ProductStyle } from './Products.style';
 
-const Products = () => {
+const Products = ({ datas }) => {
+   // console.log(nome);
    return (
       <ProductStyle>
          <Wrap>
             <h1>Produtos</h1>
+
+            <section>
+               {datas.map(({ id, nome, fotos }) => (
+                  <Card key={id} name={nome} photo={fotos} />
+               ))}
+            </section>
          </Wrap>
       </ProductStyle>
    );
