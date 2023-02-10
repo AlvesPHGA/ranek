bbb3b3f0715c830f9b009ee4f26d70b9e844104c
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-10 15:03:59 -0300

    updated product page

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Wrap } from '../../style/Styles.style';
+import { ProductStyle } from './Products.style';
 
 const Products = () => {
-   return <div>Products</div>;
+   return (
+      <ProductStyle>
+         <Wrap>
+            <h1>Produtos</h1>
+         </Wrap>
+      </ProductStyle>
+   );
 };
 
 export default Products;
