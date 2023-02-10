1c4e52713064ed9d045142bd0f10140531a1cce5
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-10 15:01:15 -0300

    updated page contact and created a styled to contact

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Wrap } from '../../style/Styles.style';
+import { ContactStyle } from './Contact.style';
 
 const Contact = () => {
-   return <div>Contact</div>;
+   return (
+      <ContactStyle>
+         <Wrap>
+            <h1>Contatos</h1>
+         </Wrap>
+      </ContactStyle>
+   );
 };
 
 export default Contact;
---
+++ src/Pages/Contact/Contact.style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ContactStyle = styled.section`
+   padding: 4rem 0;
+
+   & h1 {
+      font-size: 2rem;
+   }
+`;
