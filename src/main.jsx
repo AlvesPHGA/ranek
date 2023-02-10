588e4909994612b3a66582dee5ccc8e2cb58b668
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-10 12:26:33 -0300

    add a GlobalStyle in project

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyle } from './style/GlobalStyle.style';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
+      <GlobalStyle />
       <App />
    </React.StrictMode>,
 );
