4c0151650151ab59f328aedc8543a409f597a07e
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-13 14:54:25 -0300

    created a doc to product intern

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Wrap } from '../../style/Styles.style';
+import { HeaderStyle } from './Header.styled';
+
+const Header = () => {
+   return (
+      <HeaderStyle>
+         <Wrap>
+            <nav>
+               <NavLink to="/" end>
+                  Produtos
+               </NavLink>
+               <NavLink to="contact">Contato</NavLink>
+            </nav>
+         </Wrap>
+      </HeaderStyle>
+   );
+};
+
+export default Header;
