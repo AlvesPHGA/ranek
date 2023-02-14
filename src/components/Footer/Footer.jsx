36bcb3dbddddd3715cb0d238d1944fe197d44cd7
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-14 14:58:01 -0300

    add a footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrap } from '../../style/Styles.style';
+
+const Footer = () => {
+   return (
+      <Wrap>
+         <h1>Footer</h1>
+      </Wrap>
+   );
+};
+
+export default Footer;
